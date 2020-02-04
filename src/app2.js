34f5f7cac42d6406459a864dc410a041b3710fb6
Dima Vokov <dima.dima99.vokov@mail.ru>
2020-02-04 18:15:10 +0200

    Add private routes

--- src/app2.js
+++ src/app2.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import AppRouter from './routers/AppRouter'
-
+import AppRouter, {history} from './routers/AppRouter'
 
 import configureStore from './store/configureStore'
 import {startSetExpenses} from './actions/exprenses'
-import {setTextFilter} from './actions/filters'
+import {login, logout} from './actions/auth'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 import 'react-dates/lib/css/_datepicker.css'
-import { app } from 'firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore()
-console.log('test')
-// store.dispatch(addExpense({description: 'Water bill', amount: 3000}))
-// store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}))
-// store.dispatch(addExpense({description: 'Rent', amount: 10990}))
-
-// const state = store.getState()
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-// console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
@@ -31,8 +22,29 @@ const jsx = (
 
 const appRoot = document.getElementById('app');
 
+let hasRendered = false
+const renderApp = () => {
+    if(!hasRendered){
+        ReactDOM.render(jsx, appRoot)
+        hasRendered = true
+    }
+}
+
 ReactDOM.render(<p>Loading...</p>, appRoot)
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, appRoot)
+firebase.auth().onAuthStateChanged((user) => {
+    if(user){
+        store.dispatch(login(user.uid))
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp()
+
+            if(history.location.pathname === '/'){
+                history.push('/dashboard')
+            }
+        })
+    }else{
+        store.dispatch(logout())
+        renderApp()
+        history.push('/')
+    }
 })
